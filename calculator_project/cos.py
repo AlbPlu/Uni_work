e26eb1166cb55f0db8dbbc281513ef8c1ed18795
AlbPlu <reksis202@gmail.com>
2024-10-21 12:05:03 +0300

    Created a new folder to contain the future project

---
+++ calculator_project/cos.py
@@ -0,0 +1,7 @@
+import math
+
+def trig_cos(a):
+	return math.cos(a)
+
+a = float(input("enter number: "))
+print(trig_cos(a))
---
+++ calculator_project/subtraction.py
@@ -0,0 +1,6 @@
+def subtraction(a, b):
+	return (a - b)
+
+a = float(input("enter first number: "))
+b = float(input("enter second number: "))
+print(subtraction(a,b))
