05afbb41c2c0202e426eb74cbd87007210ec4b35
AlbPlu <reksis202@gmail.com>
2024-10-21 12:16:40 +0300

    Removed expo.py from base directroy after transfering

--- expo.py
+++
@@ -1,9 +0,0 @@
-def exp(base, a):
-	if (base == 0) and (a == 0):
-		return ("0^0 is inderminate")
-	else:
-		return base ** a
-
-base = float(input("enter base value: "))
-a = float(input("enter exponent value: "))
-print (exp(base,a))
